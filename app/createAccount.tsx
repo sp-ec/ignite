9a94e2d719ef154d113d46b4bb20b85d5eb30d43
catherine-tomic <cattomic@gmail.com>
2026-02-14 02:40:48 -0500

    Create Account Works -ish
    does not route to screen

--- app/createAccount.tsx
+++ app/createAccount.tsx
@@ -1,17 +1,32 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
-
 import {
   Button,
-  ButtonText,
-  ButtonSpinner,
-  ButtonIcon,
-  ButtonGroup,
+  ButtonText
 } from "@/components/ui/button";
-import { VStack } from "@/components/ui/vstack";
 import { Input, InputField } from "@/components/ui/input";
+import { VStack } from "@/components/ui/vstack";
+import { Link, router } from "expo-router";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { View } from "react-native";
+import { auth } from "../FirebaseConfig";
+
 
 export default function createAccount() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const createAccount = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      if (user) {
+        router.replace("../(protected)/(tabs)/(swipe)/swipe.tsx"); // FIXME: send to profile
+      }
+    } catch (error: any) {
+      console.log(error);
+      alert("Sign in failed: " + error.message);
+    }
+  };
+
   return (
     <View
       style={{
@@ -27,12 +42,12 @@ export default function createAccount() {
           </ButtonText>
         </Button>
         <Input className="mb-2 text-zinc-900">
-          <InputField placeholder="Email" className="text-black" />
+          <InputField placeholder="Email" className="text-black" value={email} onChangeText={setEmail}/>
         </Input>
         <Input className="mb-2 text-zinc-900">
-          <InputField placeholder="Password" className="text-black" />
+          <InputField placeholder="Password" className="text-black" value={password} onChangeText={setPassword}/>
         </Input>
-        <Button className="bg-purple-500">
+        <Button className="bg-purple-500" onPress={createAccount}>
           <ButtonText className="text-zinc-200 text-md">
             Create Account
           </ButtonText>
