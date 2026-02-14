eaa0abe7f5ea06fe9a25e9a6725cc67731019425
catherine-tomic <cattomic@gmail.com>
2026-02-14 11:43:25 -0500

    Firestore setup

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,3 +22,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage();
+export const db = getFirestore(app);
