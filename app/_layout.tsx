0a21121e6ce4f6e5f90c5a8e583c78d211491cb4
spec <48649161+sp-ec@users.noreply.github.com>
2026-02-14 00:44:07 -0500

    yea

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,23 +7,21 @@ import {
 import { AuthProvider } from "@/utils/authContext";
 import { use, useEffect } from "react";
 
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-import '@/global.css';
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/global.css";
 
 export default function RootLayout() {
   return (
-    
     <GluestackUIProvider mode="dark">
       <AuthProvider>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="(protected)" options={{ headerShown: false }} />
-      </Stack>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+        </Stack>
+      </AuthProvider>
     </GluestackUIProvider>
-  
-    </AuthProvider>
   );
 }
