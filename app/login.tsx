3bb30a591602e1aaac3434c9743404ded7c4133e
Spectrobyte <48649161+Spectromeda@users.noreply.github.com>
2026-02-14 21:23:00 -0500

    updated login page

--- app/login.tsx
+++ app/login.tsx
@@ -10,6 +10,7 @@ import {
 import { useState, useEffect } from "react";
 import { ActivityIndicator, SafeAreaView, View } from "react-native";
 import { auth } from "../FirebaseConfig";
+import { Text } from "@/components/ui/text";
 
 export default function Login() {
 	const [email, setEmail] = useState("");
@@ -61,34 +62,39 @@ export default function Login() {
 				alignItems: "center",
 			}}
 		>
-			<VStack>
-				<Input className="mb-2 text-zinc-900">
+			<Text className="text-3xl font-extrabold mb-2">Dating App</Text>
+			<Text className="mb-32 ">Making connections easier.</Text>
+			<VStack className="w-64">
+				<Input className="mb-4 text-zinc-900" size="xl">
 					<InputField
 						placeholder="Email"
 						className="text-black"
 						value={email}
 						onChangeText={setEmail}
+						size="xl"
 					/>
 				</Input>
-				<Input className="mb-2 text-zinc-900">
+				<Input className="mb-4 text-zinc-900" size="xl">
 					<InputField
 						placeholder="Password"
 						className="text-black"
 						value={password}
 						onChangeText={setPassword}
+						size="xl"
 					/>
 				</Input>
-				<Button className="bg-purple-500 mb-2 " onPress={signIn}>
-					<ButtonText className="text-zinc-200 text-md">Log in</ButtonText>
+				<Button className="bg-purple-600 mb-4 " size="xl" onPress={signIn}>
+					<ButtonText className="text-zinc-200 text-lg">Log in</ButtonText>
 				</Button>
 
 				<Button
-					className="text-md bg-zinc-200 mb-2"
+					className="text-lg bg-zinc-200 mb-4"
 					onPress={() => {
 						router.navigate("/createAccount");
 					}}
+					size="xl"
 				>
-					<ButtonText className="text-zinc-900 text-md">
+					<ButtonText className="text-zinc-900 text-lg">
 						Create Account
 					</ButtonText>
 				</Button>
